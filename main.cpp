d0ccf2c3fdf188158f7a9a9fbb9be924b98aa255
owfxtcch <owfxtcch@users.noreply.github.com>
2014-11-01 00:51:48 +0100

    Lex graph dumping capabilities added.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <cstdlib>
 
 #include <string.h>
 
@@ -10,37 +11,45 @@
 #include "lexer/token.hpp"
 #include "lexer/lexer.hpp"
 
-int main(int argc, const char *argv[])
-{
-	bool debug = false;
-	if (argc == 3 && strncmp(argv[1], "--lextest", 10) == 0)
-	{
-		debug = true;
-	}
+int main(int argc, const char *argv[]) {
 
-	std::ifstream infile(argv[2]);
-	if (!infile.good())
-	{
-		std::cerr << "Error reading file." << std::endl;
-		return 1;
-	}
+    if (argc == 3 && std::string(argv[1]) == "--dumplexgraph") {
+        Stateomat stateomat;
+        stateomat.dump_graph(std::string(argv[2]));
+    }
+    else {
+        bool debug = false;
+        if (argc == 3 && strncmp(argv[1], "--lextest", 10) == 0)
+        {
+            debug = true;
+        }
 
-	Stateomat stateomat;
-	Lexer lexer(infile, stateomat, debug);
-	token t = lexer.get_next_token();
-	while (t.type != TOKEN_ERROR)
-	{
-		if (t.type == TOKEN_EOF)
-		break;
-		t = lexer.get_next_token();
-	}
+        std::ifstream infile(argv[2]);
+        if (!infile.good())
+        {
+            std::cerr << "Error reading file." << std::endl;
+            return 1;
+        }
 
-	infile.close();
+        Stateomat stateomat;
+        Lexer lexer(infile, stateomat, debug);
+        token t = lexer.get_next_token();
+        while (t.type != TOKEN_ERROR)
+        {
+            if (t.type == TOKEN_EOF)
+                break;
+            t = lexer.get_next_token();
+        }
 
-	if (t.type != TOKEN_EOF)
-	{
-		std::cerr << "Error: Lexer failed." << std::endl;
-		return 1;
-	}
-	return 0;
+        infile.close();
+
+        if (t.type != TOKEN_EOF)
+        {
+            std::cerr << "Error: Lexer failed." << std::endl;
+            return 1;
+        }
+        return 0;
+    }
+
+    return EXIT_SUCCESS;
 }
