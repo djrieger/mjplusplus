2f29d311456381c49622f9a47484d2c7173dfb2d
owfxtcch <owfxtcch@users.noreply.github.com>
2014-11-05 17:41:39 +0100

    Added parser functions from UnaryExpression and below.

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -6,43 +6,59 @@
 
 class Parser
 {
-    protected:
-        // TODO: determine member variables.
-        Lexer& lexer;
-        bool print_messages;
-        Token current;
-        std::string error_msg_buf;
+	protected:
+		// TODO: determine member variables.
+		Lexer& lexer;
+		bool print_messages;
+		Token current;
+		std::string error_msg_buf;
 
-        Token nextToken();
-        bool expect(Token::Token_type tokenType);
-    public:
-        /**
-         * Constructor to the parser, needs at least a lexer.
-         * Since it's going to be handimplemented, the grammar is implicitly defined in the the functions
-         * @param	lexer	instance of a lexer providing the tokens
-         * @param   print_messages   sets whether error messages will be printed, default: true
-         */
-        Parser(Lexer& lexer, bool print_messages=true); // TODO: determine parameters
-		
-        /**
-         * Generic function to start the parser
-         */
-        bool start(); 
+		Token nextToken();
+		bool expect(Token::Token_type tokenType);
+	public:
+		/**
+		 * Constructor to the parser, needs at least a lexer.
+		 * Since it's going to be handimplemented, the grammar is implicitly defined in the the functions
+		 * @param	lexer	instance of a lexer providing the tokens
+		 * @param   print_messages   sets whether error messages will be printed, default: true
+		 */
+		Parser(Lexer& lexer, bool print_messages = true); // TODO: determine parameters
 
-        /**
-         * Prints the content of the error message buffer and resets it to an empty string. TODO: private?
-         */
-        void printError();
+		/**
+		 * Generic function to start the parser
+		 */
+		bool start();
 
-    private:
-        // TODO: parseNT() function for each non terminal
-        // TODO: determine (in and out) parameters as well as the return type, e.g.: anchor set for panic mode, later on the AST data structure.
-        bool parseProgram();
-        bool parseClassDeclaration();
+		/**
+		 * Prints the content of the error message buffer and resets it to an empty string. TODO: private?
+		 */
+		void printError();
+
+	private:
+		// TODO: parseNT() function for each non terminal
+		// TODO: determine (in and out) parameters as well as the return type, e.g.: anchor set for panic mode, later on the AST data structure.
+		bool parseProgram();
+		bool parseClassDeclaration();
 		bool parseExpression();
 		bool parseAssignmentExpression();
 		bool precedenceClimb(int minPrec);
-        // ..
+		// ..
+		bool parsePrimaryExpression();
+		bool parseNewObjectOrNewArrayExpression();
+		bool parseNewObjectExpression();
+		bool parseNewArrayExpression();
+		bool parseOptionalBrackets();
+		bool parseNewKeyword();
+		bool parseArgumentsExpressions();
+		bool parseArrayAccess();
+		bool parseArguments();
+		bool parseMethodInvocation();
+		bool parseMethodInvocationOrFieldAccess();
+		bool parseExclMarkOrHyphen();
+		bool parsePostfixExpression();
+		bool parsePostfixOps();
+		bool parsePostfixOp();
+		bool parseUnaryExpression();
 };
 
 #endif
