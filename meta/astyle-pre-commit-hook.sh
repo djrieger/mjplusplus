863872e0c2828ac874906c048b7fd13aef6ca89a
David Rieger <djrieger@users.noreply.github.com>
2014-11-05 23:49:15 +0100

    Removed comment from astyle hook script

--- meta/astyle-pre-commit-hook.sh
+++ meta/astyle-pre-commit-hook.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# Unresolved issues:
-# 1) When only changing the formatting of a file and comitting, this will
-# result in an empty commit since astyle changes the file to the version
-# of the previous commit.
-# 2) Staging hunks instead of whole files is a potentail pitfall when
-# doing hooked auto-formatting with astyle
 
 # Get all changed cpp and hpp files for this commit
 CHANGEDFILES=`git diff --cached --name-only | egrep '\.cpp$|\.hpp$'`
