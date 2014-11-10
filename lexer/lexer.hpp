5d175c641164ae0a5e64f1fd01b78b69ab6770b9
David Rieger <djrieger@users.noreply.github.com>
2014-11-10 08:25:18 +0100

    Increased readability (Lexer)

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -16,6 +16,17 @@ class Lexer
 		bool debug;
 		std::vector<Token> token_stack;
 
+		/*
+		 * If nextCharacter is \n the line number in position will be incremented
+		 * and column is reset to 1. Otherwise column will be incremented.
+		 */
+		void advancePosition(char nextCharacter);
+		/*
+		 * If nextCharacter is \n the line number in position will be decreased by one.
+		 * Otherwise the column will be decreased by one.
+		 */
+		void rewindPosition(char nextCharacter);
+
 	public:
 		Lexer(std::istream& input, Stateomat const& stateomat, bool debug = false);
 		Token get_next_token();
