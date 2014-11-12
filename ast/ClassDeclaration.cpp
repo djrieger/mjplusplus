36cfe74169ef63731a713e59e50fe4b3942b970e
Nidan <nidan-github@ghostdub.de>
2014-11-12 18:40:01 +0100

    ast fixing

---
+++ ast/ClassDeclaration.cpp
@@ -0,0 +1,18 @@
+#include "ClassDeclaration.hpp"
+
+namespace ast
+{
+	ClassDeclaration::ClassDeclaration(Ident& class_name, std::vector<ClassMember> members) : class_name(class_name), members(members)
+	{
+		;
+	}
+	std::string ClassDeclaration::toString()
+	{
+		std::string r("class " + class_name.toString() + std::endl + '{');
+
+		for (auto it = members.begin(); it != member.end; it++)
+			r += it->toString();
+
+		r += '}';
+	}
+}
