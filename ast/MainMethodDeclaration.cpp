77e047544bd835f1f8544f820ec74a5c1b615319
Peter Wolf <pwolf2310@gmail.com>
2014-11-12 19:30:40 +0100

    fixed constructor

--- ast/MainMethodDeclaration.cpp
+++ ast/MainMethodDeclaration.cpp
@@ -1,15 +1,29 @@
 #include "MainMethodDeclaration.hpp"
+#include "TypeIdent.hpp"
 
-ast::MainMethodDeclaration::MainMethodDeclaration(Statement& block)
-	: MethodDeclaration(block)
+
+ast::MainMethodDeclaration::MainMethodDeclaration(TypeIdent& return_type_and_name, std::vector<TypeIdent> parameters, Statement& block)
+	: MethodDeclaration(return_type_and_name, parameters, block)
 {
 	;
 }
 
 std::string ast::MainMethodDeclaration::toString()
 {
-	std::string ret = "public static void main(String args[])\n";
-	ret += block.toString();
+	std::string ret = "public static " + return_type_and_name.toString() + "(";
+	auto it = parameters.begin();
+
+	while (1)
+	{
+		ret += it->toString();
+
+		if (++it != parameters.end())
+			ret += ", ";
+		else
+			break;
+	}
+
+	ret += ")\n" + block.toString();
 
 	return ret;
 }
