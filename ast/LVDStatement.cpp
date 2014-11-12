114222ba0df1474a93e7d37304ba84496339b3e3
Peter Wolf <pwolf2310@gmail.com>
2014-11-12 18:03:09 +0100

    Added rudimentary .cpp for some AST-files and updated the header with include-guards.

---
+++ ast/LVDStatement.cpp
@@ -0,0 +1,13 @@
+#include "LVDStatement.hpp"
+
+ast::LVDStatement::LVDStatement(TypeIdent& type_ident)
+	: type_ident(type_ident)
+{
+	
+}
+
+
+std::string ast::LVDStatement::toString() 
+{
+	return type_ident.toString() + ";" + std::endl;
+}
---
+++ ast/MainMethodDeclaration.cpp
@@ -0,0 +1,17 @@
+#include "MainMethodDeclaration.hpp"
+
+ast::MainMethodDeclaration::MainMethodDeclaration(Statement& block)
+	: ast::MethodDeclaration::MethodDeclaration(block);
+{
+	
+}
+
+std::string ast::MainMethodDeclaration::toString() 
+{
+	std::string ret = "public static void main(String args[])" + std::endl;
+	ret += block.toString();
+		
+	return ret;
+}
+
+
