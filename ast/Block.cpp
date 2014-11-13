5bbdf62f6e8441d166d6351b11f8cc95dd22141f
Peter Wolf <pwolf2310@gmail.com>
2014-11-13 18:54:12 +0100

    added an AST-node for Block

---
+++ ast/Block.cpp
@@ -0,0 +1,21 @@
+#include "Block.hpp"
+
+namespace ast
+{
+	Block::Block(std::unique_ptr<std::vector<std::unique_ptr<Statement>>>& block_statements)
+		: block_statements(std::move(block_statements))
+	{
+		;
+	}
+	std::string Block::toString()
+	{
+		std::string r("{");
+
+		for (auto it = block_statements->begin(); it != block_statements->end(); it++)
+			r += (**it).toString();
+
+		r += '}';
+
+		return r;
+	}
+}
