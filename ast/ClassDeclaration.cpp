33960744a351a6fd3553392ccef2424d8aae2c81
David Rieger <djrieger@users.noreply.github.com>
2014-11-13 15:29:54 +0100

    Made attributes of ast/ClassDeclaration unique_ptrs

--- ast/ClassDeclaration.cpp
+++ ast/ClassDeclaration.cpp
@@ -2,13 +2,13 @@
 
 namespace ast
 {
-	ClassDeclaration::ClassDeclaration(Ident class_name, std::unique_ptr<std::vector<ClassMember>>& members) : class_name(class_name), members(std::move(members))
+	ClassDeclaration::ClassDeclaration(std::unique_ptr<Ident>& class_name, std::unique_ptr<std::vector<ClassMember>>& members) : class_name(std::move(class_name)), members(std::move(members))
 	{
 		;
 	}
 	std::string ClassDeclaration::toString()
 	{
-		std::string r("class " + class_name.toString() + "\n{");
+		std::string r("class " + class_name->toString() + "\n{");
 
 		for (auto it = members->begin(); it != members->end(); it++)
 			r += it->toString();
