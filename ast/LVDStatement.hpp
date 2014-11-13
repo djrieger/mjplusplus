66f0f7144d76f69677e180e3568dcaf549460541
Nidan <nidan-github@ghostdub.de>
2014-11-13 16:31:49 +0100

    adding uniqueness from If to Main

--- ast/LVDStatement.hpp
+++ ast/LVDStatement.hpp
@@ -1,6 +1,7 @@
 #ifndef LVD_STATEMENT_HPP
 #define LVD_STATEMENT_HPP
 
+#include "Expression.hpp"
 #include "Statement.hpp"
 #include "TypeIdent.hpp"
 
@@ -9,10 +10,12 @@ namespace ast
 	class LVDStatement : public Statement
 	{
 		protected:
-			TypeIdent& type_ident;
+			std::unique_ptr<TypeIdent> type_ident;
+			std::unique_ptr<Expression> init_expr;
 
 		public:
-			LVDStatement(TypeIdent& type_ident);
+			LVDStatement(std::unique_ptr<TypeIdent>& type_ident);
+			LVDStatement(std::unique_ptr<TypeIdent>& type_ident, std::unique_ptr<Expression>& init_expr);
 			virtual std::string toString();
 	};
 }
