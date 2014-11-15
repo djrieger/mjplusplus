e128d0a71fe4c6ba9581a03ba76ee0c477c0cb75
David Rieger <djrieger@users.noreply.github.com>
2014-11-15 14:57:02 +0100

    Added missing semicolon at the end of ExpressionStatement

--- ast/ExpressionStatement.cpp
+++ ast/ExpressionStatement.cpp
@@ -7,5 +7,5 @@ ast::ExpressionStatement::ExpressionStatement(std::unique_ptr<Expression>& expr)
 
 std::string ast::ExpressionStatement::toString(unsigned int indent) const
 {
-	return std::string(indent, '\t') + expression->toString(indent) + '\n';
+	return std::string(indent, '\t') + expression->toString(indent) + ";\n";
 }
