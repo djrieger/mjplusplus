acf33a950067afbf1fa04394102fa78b51d4d8d8
Maximilian Vogel <maxvogel@users.noreply.github.com>
2014-11-17 22:04:58 +0100

    added option parser and adapted main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,51 @@
 #include "lexer/stateomat.hpp"
 #include "lexer/token.hpp"
 #include "parser/parser.hpp"
+#include "util/optionparser.h"
 
-#define CMD_LINE_OPTION_PREFIX "--"
+enum optionIndex {UNKNOWN, HELP, DUMPLEXGRAPH, LEXTEST, PRINT_AST, CHECK};
+const option::Descriptor usage[] =
+{
+	{UNKNOWN, 0, "", "", option::Arg::None, "USAGE: mj++ [option] FILE\n\nOptions:"},
+	{HELP, 0, "-h", "help", option::Arg::None, "  --help, -h\tPrint usage and exit."},
+	{DUMPLEXGRAPH, 0, "", "dumplexgraph", option::Arg::None, "  --dumplexgraph\tPrint automaton of lexer to the given file name."},
+	{LEXTEST, 0, "", "lextest", option::Arg::None, "  --lextest\tOnly run the lexer on the input FILE."},
+	{PRINT_AST, 0, "", "print-ast", option::Arg::None, "  --print-ast\tPretty prints the content of the abstract syntax tree after a file has been parsed."},
+	{CHECK, 0, "", "check", option::Arg::None, "  --check\tRuns the semantic analysis"},
+	{UNKNOWN, 0, "", "", option::Arg::None, "If no option is given, the parser will be run in silent mode."},
+	{0, 0, 0, 0, 0, 0}
+};
 
 int main(int argc, const char** argv)
 {
-	if (argc == 1)
+	argc -= (argc > 0);
+	argv += (argc > 0); // skip program name argv[0] if present
+	option::Stats stats(usage, argc, argv);
+	option::Option options[stats.options_max], buffer[stats.buffer_max];
+	option::Parser parse(usage, argc, argv, options, buffer);
+
+	if (parse.error())
+		// optionally print error message
+		return EXIT_FAILURE;
+
+	if (options[HELP])
 	{
-		std::cerr << "No file specified for compilation." << std::endl;
+		option::printUsage(std::cout, usage);
+		return EXIT_SUCCESS;
+	}
+
+	if (argc == 0)
+	{
+		std::cerr << "No file specified for compilation. Use -h or --help for help." << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	std::vector<std::string> options;
+	std::string file_name = parse.nonOption(0);
+	Stateomat stateomat;
 
-	for (int i = 1; i < argc - 1; i++)
-		options.push_back(argv[i]);
-
-	std::string file_name = argv[argc - 1];
-	auto has_option = [&options] (std::string option)
+	if (options[DUMPLEXGRAPH])
 	{
-		return std::find(options.begin(), options.end(), CMD_LINE_OPTION_PREFIX + option) != options.end();
-	};
-
-	if (has_option("dumplexgraph"))
-	{
-		Stateomat stateomat;
-
+		//Stateomat stateomat;
 		try
 		{
 			stateomat.dump_graph(file_name);
@@ -45,36 +64,52 @@ int main(int argc, const char** argv)
 		return EXIT_SUCCESS;
 	}
 
-	Stateomat stateomat;
-	Lexer lexer(file_name.c_str(), stateomat);
-
-	if (has_option("lextest"))
+	try
 	{
-		Token t(lexer.get_next_token());
+		Lexer lexer(file_name.c_str(), stateomat);
 
-		while (t.token_type != Token::Token_type::TOKEN_ERROR && t.token_type != Token::Token_type::TOKEN_EOF)
+
+		if (options[LEXTEST])
 		{
-			t.print();
-			t = lexer.get_next_token();
+			Token t(lexer.get_next_token());
+
+			while (t.token_type != Token::Token_type::TOKEN_ERROR && t.token_type != Token::Token_type::TOKEN_EOF)
+			{
+				t.print();
+				t = lexer.get_next_token();
+			}
+
+			if (t.token_type != Token::Token_type::TOKEN_EOF)
+			{
+				std::cerr << "Error: Lexer failed at line " << t.position.first << ", column " << t.position.second << std::endl;
+				return EXIT_FAILURE;
+			}
+
+			return EXIT_SUCCESS;
 		}
 
-		if (t.token_type != Token::Token_type::TOKEN_EOF)
+		if (options[CHECK])
 		{
-			std::cerr << "Error: Lexer failed at line " << t.position.first << ", column " << t.position.second << std::endl;
+			std::cout << "Not implemented yet, exiting..." << std::endl;
+			return EXIT_SUCCESS;
+		}
+
+		Parser parser(lexer, true);
+		bool valid = parser.start();
+
+		if (!valid)
 			return EXIT_FAILURE;
-		}
+
+		if (options[PRINT_AST])
+			parser.getRoot()->toString(std::cout, 0);
 
 		return EXIT_SUCCESS;
+
+	}
+	catch (std::string msg)
+	{
+		std::cout << msg << std::endl;
+		return EXIT_FAILURE;
 	}
 
-	Parser parser(lexer, true);
-	bool valid = parser.start();
-
-	if (!valid)
-		return EXIT_FAILURE;
-
-	if (has_option("print-ast"))
-		parser.getRoot()->toString(std::cout, 0);
-
-	return EXIT_SUCCESS;
 }
