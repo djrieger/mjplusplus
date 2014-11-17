2096143e23b598a9ac56f0a95d85cf472445d3de
Simon Bischof <uedkm@student.kit.edu>
2014-11-17 12:28:19 +0100

    commented lexer and related files

--- lexer/stateomat.hpp
+++ lexer/stateomat.hpp
@@ -15,17 +15,33 @@
 class Stateomat
 {
 	public:
+		/** Constructor. */
 		Stateomat();
+		/**
+		 *  Returns whether the current state is accepting.
+		 *  @param state the state to test
+		 *  @returns true if state is accepting, else false
+		 */
 		bool state_is_accepting(unsigned int state);
+		/**
+		 * Writes the stateomat transition graph as a gml-file.
+		 * @param file_name file to write the graph into
+		 */
 		void dump_graph(std::string const& file_name);
 
 		//protected:
+		/** transition map */
 		std::vector<std::vector<unsigned int>> transitions;
+		/** saves the token types corresponding to the different states */
 		std::vector<Token::Token_type> state_type;
-
+		/** maps string representation to token type for all keywords */
 		std::unordered_map<std::string, Token::Token_type> keywords;
+		/**
+		 *  maps string representation to token type for operators.
+		 *  indexed by end state to get the appropriate map
+		 */
 		std::vector<std::unordered_map<std::string, Token::Token_type>> operators;
-
+		/** maps token type to its string representation (for printing purposes) */
 		std::map<Token::Token_type, std::string> reverse;
 };
 
