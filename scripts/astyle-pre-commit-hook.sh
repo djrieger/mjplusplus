d7e65983cdc5c93f2ecc50930e33827662a56b7d
David Rieger <djrieger@users.noreply.github.com>
2014-11-18 23:48:24 +0100

    Added hashes of email addresses to pre-commit hook which must not be for commit author information. Future commits using these email addresses will be prevented.

--- scripts/astyle-pre-commit-hook.sh
+++ scripts/astyle-pre-commit-hook.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# david david
+PROHIBITEDEMAILHASHES="f2151a35773b112c3cdec537203dac46 e9ff8469316bf427550ef651afbb8c3f"
+GITUSEREMAIL=$(git config user.email)
+GITUSEREMAILHASH=$(md5 -qs ${GITUSEREMAIL})
+if [[ $PROHIBITEDEMAILHASHES =~ "${GITUSEREMAILHASH}" ]]; then
+	echo "${GITUSEREMAIL} not allowed to commit. Aborting."
+	exit 1
+fi 
 
 # Get all changed cpp and hpp files for this commit
 CHANGEDFILES=`git diff --cached --name-only | egrep '\.cpp$|\.hpp$'`
