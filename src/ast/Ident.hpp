763f99a4faa1678672fc90bfc6eba55f8b1786ae
Simon Bischof <uedkm@student.kit.edu>
2014-11-19 17:11:01 +0100

    Added operator== for types (and other useful methods). Fixes #16.

--- src/ast/Ident.hpp
+++ src/ast/Ident.hpp
@@ -15,6 +15,8 @@ namespace ast
 			Ident(std::string const& string_value);
 			virtual void toString(std::ostream& out, unsigned int indent, bool = false) const;
 			virtual std::string getName() const;
+			bool operator==(Ident const& other);
+			bool operator!=(Ident const& other);
 	};
 }
 #endif // IDENT_HPP
