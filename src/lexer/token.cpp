44ccd2ff36b9f94fdbadf501d5321b05ef9d1dcc
Nidan <nidan-github@ghostdub.de>
2014-11-19 12:51:16 +0100

    fixed --lextest printing pointers

--- src/lexer/token.cpp
+++ src/lexer/token.cpp
@@ -27,18 +27,18 @@ void Token::print() const
 			break;
 
 		case TOKEN_INT_LIT:
-			std::cout << "integer literal " << string_value << std::endl;
+			std::cout << "integer literal " << *string_value << std::endl;
 			break;
 
 		case TOKEN_IDENT:
-			std::cout << "identifier " << string_value << std::endl;
+			std::cout << "identifier " << *string_value << std::endl;
 			break;
 
 		case TOKEN_ERROR:
 			break;
 
 		default:
-			std::cout << string_value << std::endl;
+			std::cout << *string_value << std::endl;
 			break;
 	}
 }
