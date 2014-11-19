291995e2032b9981a736cd2e7306fb5709b8a118
Peter Wolf <pwolf2310@gmail.com>
2014-11-19 16:48:59 +0100

    closes #15

--- src/ast/UnaryExpression.cpp
+++ src/ast/UnaryExpression.cpp
@@ -1,37 +1,106 @@
 #include "../globals.hpp"
 #include "UnaryExpression.hpp"
 
-ast::UnaryExpression::UnaryExpression(uptr<Expression>& child, uptr<vec<Unary_Operator>>& unary_operators) :
-	child(std::move(child)), unary_operators(std::move(unary_operators))
+namespace ast
 {
-
-}
-
-void ast::UnaryExpression::toString(std::ostream& out, unsigned int indent, bool special) const
-{
-	bool last_was_minus = false;
-
-	if (!special && !unary_operators->empty())
-		out << '(';
-
-	for (auto& unary_operator : *unary_operators)
+	namespace ue
 	{
-		switch (unary_operator)
+		UnaryExpression::UnaryExpression(uptr<Expression> child, int size)
+			: child(std::move(child)), size(size)
 		{
-			case UnaryExpression::Unary_Operator::UNARY_NOT:
-				out << "!";
-				last_was_minus = false;
-				break;
 
-			case UnaryExpression::Unary_Operator::UNARY_MINUS:
-				out << (last_was_minus ? " -" : "-");
-				last_was_minus = true;
-				break;
 		}
+
+		uptr<Expression> UnaryExpression::createUnaryExpr(uptr<Expression> child, uptr<vec<Token::Token_type>> operator_types)
+		{
+			if (!operator_types->empty())
+			{
+				Token::Token_type last_type = Token::Token_type::TOKEN_ERROR;
+				int size = 0;
+
+				for (auto rit = operator_types->rbegin(); rit != operator_types->rend(); ++rit)
+				{
+
+					Token::Token_type current_type = *rit;
+
+					if (current_type == last_type)
+						size++;
+					else if (size == 0)
+					{
+						last_type = current_type;
+						size++;
+					}
+					else
+					{
+						if (last_type == Token::Token_type::OPERATOR_MINUS)
+							child = std::make_unique<Neg>(std::move(child), size);
+						else if (last_type == Token::Token_type::OPERATOR_NOT)
+							child = std::make_unique<Not>(std::move(child), size);
+
+						size = 1;
+						last_type = current_type;
+					}
+				}
+
+				if (last_type == Token::Token_type::OPERATOR_MINUS)
+					child = std::make_unique<Neg>(std::move(child), size);
+				else if (last_type == Token::Token_type::OPERATOR_NOT)
+					child = std::make_unique<Not>(std::move(child), size);
+			}
+
+			return child;
+		}
+
+		void UnaryExpression::toString(std::ostream& out, unsigned int indent, std::string op, bool special) const
+		{
+			if (!special)
+				out << '(';
+
+			for (int i = 0; i < size; i++)
+				out << op;
+
+			child->toString(out, indent);
+
+			if (!special)
+				out << ')';
+		}
+
+		Not::Not(uptr<Expression> child, int size)
+			: UnaryExpression::UnaryExpression(std::move(child), size)
+		{
+			;
+		}
+
+		void Not::toString(std::ostream& out, unsigned int indent, bool special) const
+		{
+			UnaryExpression::toString(out, indent, "!", special);
+		}
+
+		Neg::Neg(uptr<Expression> child, int size)
+			: UnaryExpression::UnaryExpression(std::move(child), size)
+		{
+			;
+		}
+
+		void Neg::toString(std::ostream& out, unsigned int indent, bool special) const
+		{
+			if (!special)
+				out << '(';
+
+			for (int i = 0; i < size; i++)
+			{
+				if (i != 0)
+					out << " -";
+				else
+					out << "-";
+			}
+
+			child->toString(out, indent);
+
+			if (!special)
+				out << ')';
+		}
+
 	}
 
-	child->toString(out, indent, special && unary_operators->empty());
-
-	if (!special && !unary_operators->empty())
-		out << ')';
 }
