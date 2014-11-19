823afd0b344835601cfccf6fc9c906e4d8ac4023
David Rieger <djrieger@users.noreply.github.com>
2014-11-19 23:01:44 +0100

    Fixed pre-commit script. Does not use md5 command any more and sticks to md5sum

--- scripts/astyle-pre-commit-hook.sh
+++ scripts/astyle-pre-commit-hook.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-# david david
+#!/bin/bash
+# david david          
 PROHIBITEDEMAILHASHES="f2151a35773b112c3cdec537203dac46 e9ff8469316bf427550ef651afbb8c3f 2747686ce14f2f4629913ac26ef3de82 ba697d57c56ae5ef5738f6387397dd4c b5fe2acf0b672a6329716b824cae2f88 7f3f96c777e686bdc0bb960ae7969802"
 GITUSEREMAIL=$(git config user.email)
 
-which md5 &> /dev/null 
+which md5sum &> /dev/null 
 if [ $? -eq 0 ]; then
-	GITUSEREMAILHASH=$(md5 -qs ${GITUSEREMAIL})
+	GITUSEREMAILHASH=$(echo -n ${GITUSEREMAIL} | md5sum | awk '{ print $1 }')
 else
-	GITUSEREMAILHASH=$(echo ${GITUSEREMAIL} | md5sum | awk '{ print $1 }')
+	echo "Please install md5sum and make sure it is in your PATH in order for author e-mail address verification to work. Aborting commit."
+  exit 1
 fi
 
 if echo "${PROHIBITEDEMAILHASHES}" | grep -q "${GITUSEREMAILHASH}"; then
