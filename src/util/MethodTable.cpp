e12b7dfa96c083458e365bb3a37c6a3842bdf92f
Simon Bischof <uedkm@student.kit.edu>
2014-11-22 18:18:18 +0100

    implemented method table, needs to be included in analysis

---
+++ src/util/MethodTable.cpp
@@ -0,0 +1,12 @@
+#include "MethodTable.hpp"
+
+bool MethodTable::insertMethod(const std::string& methodName, shptr<ast::MethodDeclaration>& node,
+						  shptr<ast::Type>&  returnType, shptr<vec<shptr<ast::Type>>>& parameterTypes)
+{
+	return methodTable.insert({methodName, {node, returnType, parameterTypes}}).second;
+}
+
+std::unordered_map<std::string, MethodTable::MethodTableItem> const& MethodTable::getMethodTable() const
+{
+	return this->methodTable;
+}
