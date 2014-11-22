656465c3d04f19ee0ebd474056fe7af6f56ecf5d
David Rieger <djrieger@users.noreply.github.com>
2014-11-22 19:06:28 +0100

    Removed unneeded method in PositionAwareNode

--- src/ast/PositionAwareNode.cpp
+++ src/ast/PositionAwareNode.cpp
@@ -4,11 +4,6 @@ ast::PositionAwareNode::PositionAwareNode(lexer::Token const& token): position(t
 {
 }
 
-void ast::PositionAwareNode::setPosition(lexer::Token const& token)
-{
-	position = token.position;
-}
-
 std::pair<unsigned int, unsigned int> ast::PositionAwareNode::getPosition() const
 {
 	return position;
