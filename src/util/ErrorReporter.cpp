4c572ecc17b468d459b274d21d0ef4cc2d3bfd84
David Rieger <djrieger@users.noreply.github.com>
2014-11-22 14:28:10 +0100

    #23 (Semantic errors now sorted and printed with line number and column)

--- src/util/ErrorReporter.cpp
+++ src/util/ErrorReporter.cpp
@@ -3,6 +3,27 @@
 void ErrorReporter::printError(ErrorReporter::ErrorType type, std::string const& error_msg, std::pair<unsigned int, unsigned int> position)
 {
 	errors.insert(std::pair<std::pair<unsigned int, unsigned int>, std::pair<ErrorReporter::ErrorType, std::string>>(position, std::pair<ErrorReporter::ErrorType, std::string>(type, error_msg)));
+}
 
-	std::cerr << "\033[1;31mSemantic error\033[0m at line " << position.first << ", column " << position.second << ": " << error_msg << std::endl;
+void ErrorReporter::printErrors() const
+{
+	for (auto& error : errors)
+	{
+		switch (error.second.first)
+		{
+			case ErrorReporter::ErrorType::SEMANTIC:
+				std::cerr << "\033[1;31mSemantic error\033[0m ";
+				break;
+
+			case ErrorReporter::ErrorType::PARSER:
+				std::cerr << "\033[1;Parser error\033[0m ";
+				break;
+
+			case ErrorReporter::ErrorType::LEXER:
+				std::cerr << "\033[1;Lexer error\033[0m ";
+				break;
+		}
+
+		std::cerr << "at line " << error.first.first << ", column " << error.first.second << ": " << error.second.second << std::endl;;
+	}
 }
