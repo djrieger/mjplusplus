0f1ab340a70060660d3f3c83f08bee428e546b65
David Rieger <djrieger@users.noreply.github.com>
2014-11-25 00:01:04 +0100

    #31 (fixed warnings)

--- src/ast/PostfixExpression.cpp
+++ src/ast/PostfixExpression.cpp
@@ -1,7 +1,7 @@
 #include "../globals.hpp"
 #include "PostfixExpression.hpp"
 
-ast::PostfixExpression::PostfixExpression(shptr<Expression> child, shptr<vec<shptr<PostfixOp>>> postfix_op) : child(child), postfix_op(postfix_op), PositionAwareNode(child->getPosition())
+ast::PostfixExpression::PostfixExpression(shptr<Expression> child, shptr<vec<shptr<PostfixOp>>> postfix_op) : PositionAwareNode(child->getPosition()), child(child), postfix_op(postfix_op)
 {
 
 }
