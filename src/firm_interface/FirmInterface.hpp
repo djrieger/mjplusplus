f5550dabf276cd304f6331b88d53a145c81db77e
David Rieger <djrieger@users.noreply.github.com>
2014-11-26 10:51:47 +0100

    Integrated libFirm

---
+++ src/firm_interface/FirmInterface.hpp
@@ -0,0 +1,15 @@
+#ifndef FIRM_INTERFACE_HPP
+#define FIRM_INTERFACE_HPP
+
+#include <libfirm/firm.h>
+#include <libfirm/irdump.h>
+#include <libfirm/irverify.h>
+
+class FirmInterface 
+{
+	public:
+		FirmInterface();
+		~FirmInterface();
+};
+
+#endif
