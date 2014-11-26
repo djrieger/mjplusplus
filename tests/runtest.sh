cb3e8724c8858dd65c76314371d969f96e0676d7
Simon Bischof <uedkm@student.kit.edu>
2014-11-26 12:05:02 +0100

    added run test script, put test case artifacts (*.S, *.out) in gitignore

---
+++ tests/runtest.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ \! -x ../mj++ ] ; then
+	echo "Where is your compiler?"
+	exit
+fi
+
+succeeded=0
+compfailed=0
+segfaults=0
+failed=0
+asmerrors=0
+
+for i in run/*.mj ; do
+	echo $i
+	#extract substring
+	prefix=$(expr $i : '\(.*\)\.mj')
+	../mj++ --check --out $prefix.S $i #> /dev/null
+	ret=$?
+	echo $ret
+	if [ $ret -eq 0 ] ; then
+		#compile
+		gcc $prefix.S -o $prefix.out
+		ret=$?
+		echo $ret
+		if [ $ret -eq 0 ] ; then
+			#check diff
+			$prefix.out | diff $prefix.check -
+			ret=$?
+			echo $ret
+			if [ $ret -eq 0 ] ; then
+				succeeded=$((succeeded + 1))
+			else
+				failed=$((failed + 1))
+			fi
+		else
+			asmerrors=$((asmerrors + 1))
+		fi
+	elif [ $ret -eq 1 ] ; then
+		compfailed=$((compfailed + 1))
+	else
+		segfaults=$((segfaults + 1))
+	fi
+done
+echo "ran" $((succeeded + failed+segfaults)) "tests:\033[1;32m" $succeeded "\033[0msucceeded,\033[1;31m" $failed "\033[0mfailed with\033[1;31m" $segfaults "\033[0msegfaults, \033[1;31m" $compfailed "\033[0mcompilation errors and\033[1;31m" $asmerrors "\033[0massembler errors."
