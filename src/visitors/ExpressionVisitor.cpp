6b0d496b216da1df9aaa731f98a30b0c2672c88e
owfxtcch <owfxtcch@noreply.users.github.com>
2014-11-27 16:34:21 +0100

    Added initial visit functions.

--- src/visitors/ExpressionVisitor.cpp
+++ src/visitors/ExpressionVisitor.cpp
@@ -1,6 +1,6 @@
 #include "ExpressionVisitor.hpp"
 
-void visit(shptr<ast::be::Plus const> plusExpr)
+void ExpressionVisitor::visit(shptr<ast::be::Plus const> plusExpr)
 {
 	plusExpr->getLeftChild()->accept(this);
 	ir_node* left = this->resultNode;
@@ -9,4 +9,112 @@ void visit(shptr<ast::be::Plus const> plusExpr)
 	ir_node* right = this->resultNode;
 
 	this->resultNode = FirmInterface::getInstance->createOperation(plusExpr, left, right);
+}
+
+// primary expressions
+void ExpressionVisitor::visit(shptr<ast::pe::Bool const> boolExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::Ident const> identExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::Integer const> integerExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::MethodInvocation const> methodInvocationExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::NewArrayExpression const> newArrayExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::NewObjectExpression const> newObjectExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::pe::Object const> objectExpr)
+{
+	;
+}
+
+// unary expressions
+void ExpressionVisitor::visit(shptr<ast::ue::Neg const> negExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::ue::Not const> notExpr)
+{
+	;
+}
+
+// binary expressions
+void ExpressionVisitor::visit(shptr<ast::be::AndAnd const> andAndExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Eq const> eqExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::EqEq const> eqEqExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::GreaterThan const> greaterThanExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::GreaterThanEq const> greaterThanEqExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Invalid const> invalidExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::LessThan const> lessThanExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::LessThanEq const> lessThanEqExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Minus const> minusExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Mod const> modExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Mult const> multExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::NotEq const> notEqExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::OrOr const> orOrExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Plus const> plusExpr)
+{
+	;
+}
+void ExpressionVisitor::visit(shptr<ast::be::Slash const> slashExpr)
+{
+	;
+}
+
+// postfix expression
+void ExpressionVisitor::visit(shptr<ast::PostfixExpression const> postfixExpression)
+{
+	;
 }
