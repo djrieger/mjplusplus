3d72d0114404041ee77a0101a5717dfdd656717d
David Rieger <djrieger@users.noreply.github.com>
2014-12-03 19:18:10 +0100

    Implemented more StatementVisitor stuff

--- src/visitors/StatementVisitor.cpp
+++ src/visitors/StatementVisitor.cpp
@@ -1,26 +1,36 @@
 #include "StatementVisitor.hpp"
+#include "ExpressionVisitor.hpp"
+
+ir_node* StatementVisitor::getResultNode() const
+{
+	return this->resultNode;
+}
 
 void StatementVisitor::visit(shptr<const ast::IfStatement> ifStatement)
 {
-	JumpTarget trueTarget, falseTarget;
-	//ifStatement->getCondition()->visit(&trueTarget, &falseTarget);
-	/*
+	shptr<JumpTarget> trueTarget;
+	shptr<JumpTarget> falseTarget;
+	ExpressionVisitor condVisitor(trueTarget, falseTarget);
+	ifStatement->getCondition()->accept(condVisitor); // TODO: Implement accept(ExpressionVisitor) for Expression subclasses
+	
 	JumpTarget exitTarget;
-	set_cur_block(trueTarget.targetNode);
-	trueStatement->visit();
-	ir_node *trueNode = ifStatement->getThenStatement()->getResultNode();
+	set_cur_block(trueTarget->targetNode);
+
+	ifStatement->getThenStatement()->accept(*this); // TODO: Implement accept(StatementVisitor) for Statement subclasses
+	ir_node *trueNode = getResultNode();
 	exitTarget.jumpFromBlock(trueNode);
 
-	set_cur_block(falseTarget.targetNode);
+	set_cur_block(falseTarget->targetNode);
 	if (ifStatement->getElseStatement())
 	{
-		elseStatement->visit();
-		ir_node *falseNode = elseStatement->getResultNode();
+		ifStatement->getElseStatement()->accept(*this); // TODO: Implement accept(StatementVisitor) for Statement subclasses
+		ir_node *falseNode = getResultNode();
+		exitTarget.jumpFromBlock(falseNode);
 	}
-	exitTarget.jumpFromBlock(falseNode);
-
+	
 	set_cur_block(exitTarget.targetNode);
-	*/
+	
+	this->resultNode = exitTarget.targetNode;
 }
 
 void StatementVisitor::visit(shptr<const ast::WhileStatement> whileStmt)
@@ -31,4 +41,16 @@ void StatementVisitor::visit(shptr<const ast::WhileStatement> whileStmt)
 void StatementVisitor::visit(shptr<const ast::ReturnStatement> returnStmt)
 {
 
+}
+
+void StatementVisitor::visit(shptr<const ast::Block> blockStmt) {
+	;
+}
+
+void StatementVisitor::visit(shptr<const ast::ExpressionStatement> exprStmt) {
+	;
+}
+
+void StatementVisitor::visit(shptr<const ast::LVDStatement> lvdStmt) {
+	;
 }
