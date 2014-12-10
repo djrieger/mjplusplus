f28324da84443e246ed8812b39eaa9ac20264874
Maximilian Vogel <maxvogel@users.noreply.github.com>
2014-12-10 13:44:45 +0100

    implemented generic worklist algorithm for FirmNodeVisitor; added debug output; added call to worklist algorihthm

--- src/firm_interface/visitors/FirmNodeVisitor.cpp
+++ src/firm_interface/visitors/FirmNodeVisitor.cpp
@@ -1,11 +1,21 @@
 #include "FirmNodeVisitor.hpp"
-
-void FirmNodeVisitor::visit(ir_node* node) 
+#include <iostream>
+void FirmNodeVisitor::visit(ir_node* node)
 {
+	std::cout << "visiting node at " << node << "\tof type ";
+
 	if (is_Const(node))
+	{
+		std::cout << "CONST";
 		visitConstNode(node);
+	}
 	else if (is_Add(node))
+	{
+		std::cout << "ADD";
 		visitAddNode(node);
+	}
+
+	std::cout << std::endl;
 	// TODO: Implement remaining node types
 	// TODO: Replace if cascade with a switch/case
 }
