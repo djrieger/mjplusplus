32a2d1f127a1d749304cdc71c79a00a028692308
David Rieger <djrieger@users.noreply.github.com>
2014-12-11 18:24:15 +0100

    Implemented Worklist class (proposal, in comments as of now)

---
+++ src/firm_interface/Worklist.hpp
@@ -0,0 +1,16 @@
+#ifndef WORKLIST_HPP
+#define WORKLIST_HPP
+/*
+namespace firm {
+	class Worklist {
+		private:
+			ir_graph* functionGraph;
+			std::queue<ir_node*> worklist;
+		public:
+			Worklist(ir_graph* functionGraph);
+			void run(std::function<void (ir_node*)> handleFunction);
+
+	};
+}
+*/
+#endif
