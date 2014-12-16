ba7da7187ff178948e32f302cd8bf7d660fee52e
David Rieger <djrieger@users.noreply.github.com>
2014-12-16 13:14:20 +0100

    Optimized optimizePhi, moved newNodes insertion functionality to GraphOptimizer, disabled all node exchanges

--- src/firm_interface/ConstantFolder.cpp
+++ src/firm_interface/ConstantFolder.cpp
@@ -5,7 +5,7 @@ namespace firm
 {
 	ConstantFolder::ConstantFolder(ir_graph* irg): GraphOptimizer(irg)
 	{
-		newNodes = std::make_shared<std::set<ir_node*>>();
+		
 	}
 
 	void ConstantFolder::optimizePhi(ir_node* node)
@@ -25,94 +25,57 @@ namespace firm
 		{
 			ir_node* pred = get_irn_n(node, i);
 
-			if (is_Const(pred))
+			// we now know, that the node is not const and
+			// therefore, operate on tarvals from the map
+			auto curTarval = (ir_tarval*)get_irn_link(pred);
+			// to compare on previous tarval later on, set
+			isConst = false;
+
+			if (curTarval == tarval_unknown)
 			{
-				// node is const, so get its value and compare with previous values
-				auto curTarval = computed_value(pred);
+				ir_printf("tarval of predecessor %F is unknown\n", pred);
+				++numUnkowns;
+				// just count the number of unknown values
+			}
+			else if (curTarval == tarval_bad)
+			{
+				ir_printf("tarval of predecessor %F is bad\n", pred);
+				// if any input is bad, were bad aswell
+				// no need to set isConst = false;
+				isBad = true;
+			}
+			else if (get_tarval_mode(curTarval) == mode_Is)
+			{
+				// were not bad, were not unknown and were integer
+				// compare the tarval with the current const val candidate
+				auto cur = get_tarval_long(curTarval);
 
-				if (get_tarval_mode(curTarval) == mode_Is)
+				if (valSet)
 				{
-					auto cur = get_tarval_long(curTarval);
-
-					if (valSet)
+					if (cur != val)
 					{
-						if (cur != val)
-						{
-							ir_printf("predecessor %F's value does not match the current value\n", pred);
-							// now we know, that we can not replace anything.
-							isBad = true;
-						}
-					}
-					else
-					{
-						ir_printf("initial value has been set to predecessor %F's value: %d\n", pred, cur);
-						// in first iteration: set const candidate.
-						val = cur;
-						valSet = true;
+						ir_printf("tarval of predecessor %F's value does not match the current value\n", pred);
+						// now we know, that we can not replace anything.
+						isBad = true;
 					}
 				}
 				else
 				{
-					// the node is const, but its mode is not integer
-					// currently, we only handle mode_Is
+					ir_printf("initial value has been set to predecessor %F's tarval: %d\n", pred, cur);
+					// in first iteration: set const candidate.
+					val = cur;
+					valSet = true;
 				}
 			}
 			else
 			{
-				// we now know, that the node is not const and
-				// therefore, operate on tarvals from the map
-				auto curTarval = (ir_tarval*)get_irn_link(pred);
-				// to compare on previous tarval later on, set
-				isConst = false;
-
-				if (curTarval == tarval_unknown)
-				{
-					ir_printf("tarval of predecessor %F is unknown\n", pred);
-					++numUnkowns;
-					// just count the number of unknown values
-				}
-				else if (curTarval == tarval_bad)
-				{
-					ir_printf("tarval of predecessor %F is bad\n", pred);
-					// if any input is bad, were bad aswell
-					// no need to set isConst = false;
-					isBad = true;
-				}
-				else if (get_tarval_mode(curTarval) == mode_Is)
-				{
-					// were not bad, were not unknown and were integer
-					// compare the tarval with the current const val candidate
-					auto cur = get_tarval_long(curTarval);
-
-					if (valSet)
-					{
-						if (cur != val)
-						{
-							ir_printf("tarval of predecessor %F's value does not match the current value\n", pred);
-							// now we know, that we can not replace anything.
-							isBad = true;
-						}
-					}
-					else
-					{
-						ir_printf("initial value has been set to predecessor %F's tarval: %d\n", pred, cur);
-						// in first iteration: set const candidate.
-						val = cur;
-						valSet = true;
-					}
-				}
-				else
-				{
-					// not unknown, not bad, not Is, so nothing we currently handle
-				}
-
+				// not unknown, not bad, not Is, so nothing we currently handle
 			}
 
 			++i;
 		}
 		while (i < predCount && !isBad);
 
-		bool changed = false;
 		ir_printf("%F:\t", node);
 
 		if (isBad)
@@ -120,47 +83,18 @@ namespace firm
 			std::cout << "is bad" << std::endl;
 			set_irn_link(node, (void*)tarval_bad);
 		}
-		else if (numUnkowns == predCount)
-		{
-			// if the number of unknown tarvals is the same as the number of predecessors
-			// we do know, that we only have unknowns and therefore set it to unknown
-			//set_irn_link(node, (void*)tarval_unknown);
-			std::cout << "is unknown" << std::endl;
-		}
-		else if (isConst ||
-		         (get_tarval_mode(prevTarval) == mode_Is && get_tarval_long(prevTarval) == val))
-		{
-			// were not unknown, were not bad and
-			// were either const or the value tarval of both iterations is the same
-			//  -> we can replace the phi node with a new const node
-			auto newConstNode = new_r_Const_long(get_irn_irg(node), mode_Is, val);
-			exchange(node, newConstNode);
-			changed = true;
-			std::cout << "is now replaced by a new const node" << std::endl;
-		}
 		else if (valSet)
 		{
 			// get_tarval_mode(prevTarval) == mode_Is && get_tarval_long(prevTarval) != val
 			// we do have a change in the tarval of the current node, so update this accordingly
 			set_irn_link(node, (void*) new_tarval_from_long(val, mode_Is));
-			changed = true;
+			
+			markOutNodesAsNew(node);
+
 			std::cout << "tarval has been updated" << std::endl;
 		}
 		else
 			std::cout << "remains totally unchanged" << std::endl;
-
-		// when exactly do we want to add successors to the worklist again=
-		if (changed)
-		{
-			// add all successors to the worklist again
-			ir_edge_t* edge = NULL;
-			foreach_out_edge(node, edge)
-			{
-				ir_node* srcNode = get_edge_src_irn(edge);
-				//ir_printf("adding srcNode= %F at %p to worklist again\n", srcNode, srcNode);
-				newNodes->insert(srcNode);
-			}
-		}
 	}
 
 	void ConstantFolder::updateTarvalForArithmeticNode(ir_node* node)
@@ -196,13 +130,7 @@ namespace firm
 				throw "updateTarvalForArithmeticNode called on illegal node";
 
 			set_irn_link(node, (void*) resultVal);
-			ir_edge_t* edge = NULL;
-			foreach_out_edge(node, edge)
-			{
-				ir_node* srcNode = get_edge_src_irn(edge);
-				ir_printf("srcNode= %F\n", srcNode);
-				newNodes->insert(srcNode);
-			}
+			markOutNodesAsNew(node);
 		}
 	}
 
@@ -210,7 +138,7 @@ namespace firm
 	{
 		//TODO: Fix segfaults
 		updateTarvalForArithmeticNode(oldNode);
-		exchange(oldNode, newNode);
+		// exchange(oldNode, newNode);
 	}
 
 	void ConstantFolder::updateTarvalAndExchangeMemory(ir_node* oldNode, ir_node* newNode)
@@ -228,14 +156,9 @@ namespace firm
 				for (auto& e : FirmInterface::getInstance().getOuts(o))
 					set_irn_n(e.first, e.second, get_irn_n(oldNode, 0));
 			}
-			else
-				exchange(o, newNode);
+			//else
+				//exchange(o, newNode);
 		}
-	}
-
-	shptr<std::set<ir_node*>> ConstantFolder::getNewNodes() const
-	{
-		return this->newNodes;
 	}
 
 	void ConstantFolder::handleConst(ir_node* node)
@@ -404,7 +327,7 @@ namespace firm
 			        || (proj_num == pn_Cond_false && relation == ir_relation::ir_relation_false))
 			{
 				// Exchange the Proj with an unconditional jump.
-				exchange(node, new_r_Jmp(get_nodes_block(child_node)));
+				//exchange(node, new_r_Jmp(get_nodes_block(child_node)));
 			}
 
 			if ((proj_num == pn_Cond_true  && relation == ir_relation::ir_relation_false)
@@ -412,7 +335,7 @@ namespace firm
 			{
 				// Exchange Proj nodes leading to dead blocks with bad blocks.
 				// TODO: Remove bad nodes.
-				exchange(node, new_r_Bad(irg, get_modeX()));
+				//exchange(node, new_r_Bad(irg, get_modeX()));
 			}
 		}
 	}
@@ -458,7 +381,7 @@ namespace firm
 		// occuring during calls to System.out.println, but this should
 		// probably be extended for more general useless conversions.
 		ir_node* child = get_irn_n(node, 0);
-
+/*
 		if (is_Conv(child))
 		{
 			ir_node* grand_child = get_irn_n(child, 0);
@@ -468,6 +391,7 @@ namespace firm
 		}
 		else if (is_Const(child))
 			exchange(node, new_r_Const_long(irg, get_irn_mode(node), get_tarval_long(computed_value(child))));
+		*/
 	}
 
 	void ConstantFolder::handle(ir_node* node)
--- src/firm_interface/GraphOptimizer.hpp
+++ src/firm_interface/GraphOptimizer.hpp
@@ -2,7 +2,8 @@
 #define GRAPH_OPTIMIZER_HPP
 
 #include <libfirm/firm.h>
-//#include "../globals.hpp"
+#include <set>
+#include "../globals.hpp"
 
 namespace firm
 {
@@ -10,9 +11,16 @@ namespace firm
 	{
 		protected:
 			ir_graph* irg;
+			shptr<std::set<ir_node*>> newNodes;
+
+			/**
+			 * add all successors to the worklist again
+			 */
+			void markOutNodesAsNew(ir_node *node);
 		public:
 			GraphOptimizer(ir_graph* irg);
 			virtual void handle(ir_node*) = 0;
+			shptr<std::set<ir_node*>> getNewNodes() const;
 	};
 }
 #endif
