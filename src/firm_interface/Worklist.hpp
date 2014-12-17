c1dca3787ca9baaa81b15ea4b96a6431afc3c24c
David Rieger <djrieger@users.noreply.github.com>
2014-12-17 12:15:28 +0100

    Fixed bug (optimizing Sub nodes led to invalid results)

--- src/firm_interface/Worklist.hpp
+++ src/firm_interface/Worklist.hpp
@@ -23,7 +23,7 @@ namespace firm
 			void cleanUp();
 			void processChildren(Node node, std::function<void (Node leftChild, Node rightChild)> fun);
 
-			void replaceGeneric(Node node);
+			bool replaceGeneric(Node node);
 			void replaceAdd(Node node);
 			void replaceSub(Node node);
 			void replaceMul(Node node);
