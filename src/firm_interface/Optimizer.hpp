4ace34b0c3eadfc7bfcd4a9f8c8603f567464e5e
Peter Wolf <pwolf2310@gmail.com>
2015-01-21 14:16:46 +0100

    Moved optimization procedure from MemberVisitor into it's own class

---
+++ src/firm_interface/Optimizer.hpp
@@ -0,0 +1,44 @@
+#ifndef OPTIMIZER_HPP
+#define OPTIMIZER_HPP
+
+#include <libfirm/firm.h>
+#include "../globals.hpp"
+
+namespace firm
+{
+	class Optimizer
+	{
+		protected:
+			ir_graph* irg;
+			bool changed;
+			unsigned int max_iterations;
+
+			/**
+			 * Optimize the function graph by folding the constants.
+			 * @return true if some optimization was made, false if no optimization was made and the graph didn't change
+			 */
+			bool foldConstants();
+
+			/**
+			 * @return true if some optimization was made, false if no optimization was made and the graph didn't change
+			 */
+			bool optimizeControlFlow();
+
+			/**
+			 * @return true if some optimization was made, false if no optimization was made and the graph didn't change
+			 */
+			bool eliminateCommonSubexpressions();
+
+			/**
+			 * Optimize the function graph by eliminating common subexpressions.
+			 * @return true if some optimization was made, false if no optimization was made and the graph didn't change
+			 */
+			bool optimizeLocal();
+
+		public:
+			Optimizer(ir_graph* irg);
+			void run();
+			bool graphWasChanged() const;
+	};
+}
+#endif
