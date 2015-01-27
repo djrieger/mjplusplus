dbbeb80b3164a7a769fa8d90a2687852aa834220
Maximilian Vogel <maxvogel@users.noreply.github.com>
2015-01-27 11:39:28 +0100

    tried to make compiling work; still has 1 error

--- src/firm_interface/optimizer/FunctionInliner.cpp
+++ src/firm_interface/optimizer/FunctionInliner.cpp
@@ -3,6 +3,7 @@
 
 namespace firm
 {
+
 	void FunctionInliner::run()
 	{
 		for (size_t i = 0; i < get_irp_n_irgs(); i++)
@@ -63,28 +64,32 @@ namespace firm
 	ir_node* FunctionInliner::createArgumentsTuple(ir_node* callNode, ir_graph* callerIrg, ir_graph* calleeIrg)
 	{
 		/* If the call has parameters, copy all parameter entities */
-		ir_entity *calleeEntity = get_irg_entity(calleeIrg); // ent
-		ir_type *calleeType = get_entity_type(calleeEntity); // mtp
+		ir_entity* calleeEntity = get_irg_entity(calleeIrg); // ent
+		ir_type* calleeType = get_entity_type(calleeEntity); // mtp
 		int n_params = get_method_n_params(calleeType);
-		if (n_params != 0) {
+
+		if (n_params != 0)
+		{
 			// TODO: necessary? and if, for what exactly?
 			//copy_parameter_entities(callNode, calleeIrg);
 		}
 
 		/* create the argument tuple */
 
-		ir_node **args_in = new ir_node*[n_params]; // ALLOCAN(ir_node*, n_params);
+		ir_node** args_in = new ir_node*[n_params]; // ALLOCAN(ir_node*, n_params);
 
-		ir_node *callingBlock = get_nodes_block(callNode); // block, post_bl
-		for (int i = n_params - 1; i >= 0; --i) {
-			ir_node *argument = get_Call_param(callNode, i); // arg
-			ir_type *parameterType = get_method_param_type(calleeType, i); // param_tp
-			ir_mode *parameterMode = get_type_mode(parameterType); // mode
+		ir_node* callingBlock = get_nodes_block(callNode); // block, post_bl
+
+		for (int i = n_params - 1; i >= 0; --i)
+		{
+			ir_node* argument = get_Call_param(callNode, i); // arg
+			ir_type* parameterType = get_method_param_type(calleeType, i); // param_tp
+			ir_mode* parameterMode = get_type_mode(parameterType); // mode
 
 			// TODO: Can this really happen?
-			if (parameterMode != get_irn_mode(argument)) {
+			if (parameterMode != get_irn_mode(argument))
 				argument = new_r_Conv(callingBlock, argument, parameterMode);
-			}
+
 			args_in[i] = argument;
 		}
 
@@ -93,11 +98,11 @@ namespace firm
 		 * Post_call is the old Call node and collects the results of the called
 		 * graph. Both will end up being a tuple. */
 		/* XxMxPxPxPxT of Start + parameter of Call */
-		ir_node *in[pn_Start_max + 1];
+		ir_node* in[pn_Start_max + 1];
 		in[pn_Start_M]              = get_Call_mem(callNode);
 		in[pn_Start_P_frame_base]   = get_irg_frame(callerIrg);
 		in[pn_Start_T_args]         = new_r_Tuple(callingBlock, n_params, args_in);
-		ir_node *pre_call = new_r_Tuple(callingBlock, pn_Start_max+1, in);
+		ir_node* pre_call = new_r_Tuple(callingBlock, pn_Start_max + 1, in);
 
 		delete[] args_in;
 
@@ -117,8 +122,8 @@ namespace firm
 		ir_printf("Inlining %+F (%+F) into %+F\n", callNode, calleeIrg, callerIrg);
 
 		relaxGraphStates(callerIrg, calleeIrg);
-		ir_node *pre_call = createArgumentsTuple(callNode, callerIrg, calleeIrg);
-		
+		ir_node* pre_call = createArgumentsTuple(callNode, callerIrg, calleeIrg);
+
 		/* --
 		   The new block gets the ins of the old block, pre_call and all its
 		   predecessors and all Phi nodes. -- */
@@ -133,15 +138,15 @@ namespace firm
 		 * node, similar for singleton nodes like NoMem and Bad.
 		 * Note: this will prohibit predecessors to be copied - only do it for
 		 *       nodes without predecessors */
-		ir_node *start_block = get_irg_start_block(calleeIrg);
+		ir_node* start_block = get_irg_start_block(calleeIrg);
 		set_irn_link(start_block, get_nodes_block(pre_call));
 		mark_irn_visited(start_block);
 
-		ir_node *start = get_irg_start(calleeIrg);
+		ir_node* start = get_irg_start(calleeIrg);
 		set_irn_link(start, pre_call);
 		mark_irn_visited(start);
 
-		ir_node *nomem = get_irg_no_mem(calleeIrg);
+		ir_node* nomem = get_irg_no_mem(calleeIrg);
 		set_irn_link(nomem, get_irg_no_mem(callerIrg));
 		mark_irn_visited(nomem);
 
@@ -154,24 +159,29 @@ namespace firm
 		irp_free_resources(irp, IRP_RESOURCE_ENTITY_LINK);
 
 		// continue with https://github.com/MatzeB/libfirm/blob/master/ir/opt/opt_inline.c#L444
+		// FIXME: determine correct return value!!!
+		return true;
 	}
 
 	/**
 	 * copy all entities on the stack frame on 1 irg to the stack frame of another.
 	 * Sets entity links of the old entities to the copies
 	 */
-	void FunctionInliner::copy_frame_entities(ir_graph *from, ir_graph *to)
+	void FunctionInliner::copy_frame_entities(ir_graph* from, ir_graph* to)
 	{
-		ir_type *from_frame = get_irg_frame_type(from);
-		ir_type *to_frame   = get_irg_frame_type(to);
+		ir_type* from_frame = get_irg_frame_type(from);
+		ir_type* to_frame   = get_irg_frame_type(to);
 		assert(from_frame != to_frame);
+
 		for (size_t i = 0, n_members = get_class_n_members(from_frame);
-		     i < n_members; ++i) {
-			ir_entity *old_ent = get_class_member(from_frame, i);
+		        i < n_members; ++i)
+		{
+			ir_entity* old_ent = get_class_member(from_frame, i);
 
 			// parameter entities are already copied and the link has been set
-			if (!is_parameter_entity(old_ent)) {
-				ir_entity *new_ent = copy_entity_own(old_ent, to_frame);
+			if (!is_parameter_entity(old_ent))
+			{
+				ir_entity* new_ent = copy_entity_own(old_ent, to_frame);
 				set_entity_link(old_ent, new_ent);
 			}
 		}
@@ -186,66 +196,83 @@ namespace firm
 	 * inlined procedure. The new entities must be in the link field of
 	 * the entities.
 	 */
-	void copy_node_inline(ir_node *node, void *env)
+	void FunctionInliner::copy_node_inline(ir_node* node, void* env)
 	{
-		ir_graph *new_irg  = (ir_graph*) env;
-		ir_node  *new_node = irn_copy_into_irg(node, new_irg);
+		ir_graph* new_irg  = (ir_graph*) env;
+		ir_node*  new_node = irn_copy_into_irg(node, new_irg);
 
 		set_irn_link(node, new_node);
-		if (is_Member(node)) {
-			ir_graph  *old_irg        = get_irn_irg(node);
-			ir_type   *old_frame_type = get_irg_frame_type(old_irg);
-			ir_entity *old_entity     = get_Member_entity(node);
+
+		if (is_Member(node))
+		{
+			ir_graph*  old_irg        = get_irn_irg(node);
+			ir_type*   old_frame_type = get_irg_frame_type(old_irg);
+			ir_entity* old_entity     = get_Member_entity(node);
 			assert(is_Member(new_node));
+
 			/* use copied entities from the new frame */
-			if (get_entity_owner(old_entity) == old_frame_type) {
-				ir_entity *new_entity = (ir_entity*)get_entity_link(old_entity);
+			if (get_entity_owner(old_entity) == old_frame_type)
+			{
+				ir_entity* new_entity = (ir_entity*)get_entity_link(old_entity);
 				assert(new_entity != NULL);
 				set_Member_entity(new_node, new_entity);
 			}
 		}
 	}
 
-	void set_preds_inline(ir_node *node, void *env)
+	void FunctionInliner::set_preds_inline(ir_node* node, void* env)
 	{
 		irn_rewire_inputs(node);
 
 		/* move constants into start block */
-		ir_node *new_node = (ir_node*)get_irn_link(node);
-		if (is_irn_start_block_placed(new_node)) {
-			ir_graph *new_irg     = (ir_graph *) env;
-			ir_node  *start_block = get_irg_start_block(new_irg);
+		ir_node* new_node = (ir_node*)get_irn_link(node);
+
+		if (is_irn_start_block_placed(new_node))
+		{
+			ir_graph* new_irg     = (ir_graph*) env;
+			ir_node*  start_block = get_irg_start_block(new_irg);
 			set_nodes_block(new_node, start_block);
 		}
 	}
 
-	void FunctionInliner::irn_rewire_inputs(ir_node *node)
+	void FunctionInliner::irn_rewire_inputs(ir_node* node)
 	{
-		ir_node *new_node = (ir_node*)get_irn_link(node);
-		if (!is_Block(node)) {
-			ir_node *block     = get_nodes_block(node);
-			ir_node *new_block = (ir_node*)get_irn_link(block);
+		ir_node* new_node = (ir_node*)get_irn_link(node);
+
+		if (!is_Block(node))
+		{
+			ir_node* block     = get_nodes_block(node);
+			ir_node* new_block = (ir_node*)get_irn_link(block);
 			set_nodes_block(new_node, new_block);
 		}
 
-		foreach_irn_in(node, i, in) {
-			ir_node *new_in = (ir_node*)get_irn_link(in);
+#define foreach_irn_in(irn, idx, pred) \
+	for (bool pred##__b = true; pred##__b;) \
+		for (ir_node const *const pred##__irn = (irn); pred##__b; pred##__b = false) \
+			for (int idx = 0, pred##__n = get_irn_arity(pred##__irn); pred##__b && idx != pred##__n; ++idx) \
+				for (ir_node *const pred = (pred##__b = false, get_irn_n(pred##__irn, idx)); !pred##__b; pred##__b = true)
+
+		foreach_irn_in(node, i, in)
+		{
+			ir_node* new_in = (ir_node*)get_irn_link(in);
 			set_irn_n(new_node, i, new_in);
 		}
+
+#undef foreach_irn_in
 
 		/* Now the new node is complete. We can add it to the hash table for CSE. */
 		//add_identities(new_node);
 	}
 
-	ir_node *FunctionInliner::irn_copy_into_irg(const ir_node *node, ir_graph *irg)
+	ir_node* FunctionInliner::irn_copy_into_irg(ir_node* node, ir_graph* irg)
 	{
-		ir_op    *op    = get_irn_op(node);
-		ir_node  *block = op != op_Block ? get_nodes_block(node) : NULL;
-		dbg_info *dbgi  = get_irn_dbg_info(node);
-		ir_mode  *mode  = get_irn_mode(node);
-		ir_node **ins   = get_irn_in(node) + 1;
+		ir_op*    op    = get_irn_op(node);
+		ir_node*  block = op != op_Block ? get_nodes_block(node) : NULL;
+		dbg_info* dbgi  = get_irn_dbg_info(node);
+		ir_mode*  mode  = get_irn_mode(node);
+		ir_node** ins   = get_irn_in(node) + 1;
 		int       arity = get_irn_arity(node);
-		ir_node  *res   = new_ir_node(dbgi, irg, block, op, mode, arity, ins);
+		ir_node*  res   = new_ir_node(dbgi, irg, block, op, mode, arity, ins);
 
 		/* copy the attributes */
 		copy_node_attr(irg, node, res);
@@ -284,7 +311,7 @@ namespace firm
 	// 		ir_node *param       = get_Call_param(call, n_param_pos);
 	// 		ir_node *sel         = new_rd_Member(dbgi, block, frame, new_ent);
 	// 		ir_node *new_mem;
-			
+
 	// 			/* Store the parameter onto the frame */
 	// 			ir_node *store = new_rd_Store(dbgi, block, call_mem, sel, param, old_type, cons_none);
 	// 			new_mem = new_r_Proj(store, mode_M, pn_Store_M);
--- src/firm_interface/optimizer/FunctionInliner.hpp
+++ src/firm_interface/optimizer/FunctionInliner.hpp
@@ -14,12 +14,12 @@ namespace firm
 			void recurse(Node node);
 			void handle(Node node);
 			void relaxGraphStates(ir_graph* callerIrg, ir_graph* calleeIrg);
-			void copy_parameter_entities(ir_node *call, ir_graph *called_graph);
-			void copy_frame_entities(ir_graph *from, ir_graph *to);
-			void copy_node_inline(ir_node *node, void *env);
-			void set_preds_inline(ir_node *node, void *env);
-			ir_node *irn_copy_into_irg(const ir_node *node, ir_graph *irg);
-			void irn_rewire_inputs(ir_node *node);
+			void copy_parameter_entities(ir_node* call, ir_graph* called_graph);
+			void copy_frame_entities(ir_graph* from, ir_graph* to);
+			static void copy_node_inline(ir_node* node, void* env);
+			static void set_preds_inline(ir_node* node, void* env);
+			static ir_node* irn_copy_into_irg(ir_node* node, ir_graph* irg);
+			static void irn_rewire_inputs(ir_node* node);
 			ir_node* createArgumentsTuple(ir_node* callNode, ir_graph* callerIrg, ir_graph* calleeIrg);
 			bool inlineFunction(Node callNode, ir_graph* calleeIrg);
 		public:
