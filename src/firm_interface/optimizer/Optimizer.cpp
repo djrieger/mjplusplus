4553d831048fa1e68785195788fa6e2c31dd574f
Maximilian Vogel <maxvogel@users.noreply.github.com>
2015-02-02 22:24:04 +0100

    forgot to commit changes to Optimizer.cpp

--- src/firm_interface/optimizer/Optimizer.cpp
+++ src/firm_interface/optimizer/Optimizer.cpp
@@ -48,8 +48,8 @@ namespace firm
 				changed = optimizeControlFlow() || changed;
 				remove_unreachable_code(irg);
 				remove_bads(irg);
-				//optimizeJumpChains();
-				FirmInterface::getInstance().outputFirmGraph(irg, "during_opt");
+				//FirmInterface::getInstance().outputFirmGraph(irg, "during_opt");
+				optimizeJumpChains();
 			}
 			while (changed && ++iterations_count < max_iterations);
 
