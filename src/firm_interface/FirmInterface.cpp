0bb0705df9ee86f89fbd435234d7d00738973721
Nidan <nidan-github@ghostdub.de>
2015-02-03 21:44:22 +0100

    don't skip creation of then/else/loop Block if they contain no statements, phis in the end block must not end up in the condition

--- src/firm_interface/FirmInterface.cpp
+++ src/firm_interface/FirmInterface.cpp
@@ -435,11 +435,12 @@ _COut_Mprintln:\n\
 
 	std::string FirmInterface::replaceDollarAndUnderscores(std::string name)
 	{
-		auto replace_string = [] (std::string& str, const std::string& from, const std::string& to)
+		auto replace_string = [] (std::string & str, const std::string & from, const std::string & to)
 		{
 			size_t start_pos = str.find(from);
-		    if(start_pos != std::string::npos)
-		    	str.replace(start_pos, from.length(), to);
+
+			if (start_pos != std::string::npos)
+				str.replace(start_pos, from.length(), to);
 		};
 
 		replace_string(name, "_", "__");
